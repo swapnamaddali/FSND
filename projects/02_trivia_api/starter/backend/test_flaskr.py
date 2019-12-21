a69eb96ab951b71b96a5f276d7137be6d6e2ad7e
swapna Maddali <sc986c@att.com>
2019-12-21 14:14:54 -0500

    code review changes for pep8 styling

--- projects/02_trivia_api/starter/backend/test_flaskr.py
+++ projects/02_trivia_api/starter/backend/test_flaskr.py
@@ -6,6 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flaskr import create_app
 from models import setup_db, Question, Category
 
+
 class TriviaTestCase(unittest.TestCase):
     """This class represents the trivia test case"""
 
@@ -14,7 +15,10 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('postgres:password@127.0.0.1:5432', self.database_name)
+        self.database_path = "postgres://{}/{}".format(
+                                        'postgres:password@127.0.0.1:5432',
+                                        self.database_name
+                                        )
         setup_db(self.app, self.database_path)
         # binds the app to the current context
         with self.app.app_context():
@@ -29,7 +33,8 @@ class TriviaTestCase(unittest.TestCase):
 
     """
     TODO
-    Write at least one test for each test for successful operation and for expected errors.
+    Write at least one test for each test for successful operation
+    and for expected errors.
     """
 
     def test_get_categories(self):
@@ -65,11 +70,16 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(data['success'], False)
         self.assertEqual(data['message'], "Resource Not Found.")
 
-
     def test_create_questions(self):
         """Test API to create new question for trivia """
-        res = self.client().post('/questions',json={"question":"What is chemical composition of water",
-        "answer":"H2O","category":1,"difficulty":2})
+        res = self.client().post('/questions',
+                                 json={
+                                        "question": "What is chemical \
+                                                    composition of water",
+                                        "answer": "H2O",
+                                        "category": 1,
+                                        "difficulty": 2
+                                      })
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
@@ -77,15 +87,22 @@ class TriviaTestCase(unittest.TestCase):
 
     def test_405_create_question_not_allowed(self):
         """TEST API to post a question with URL that is not allowed """
-        res = self.client().post('/questions/95',json={"question":"What is chemical composition of water",
-        "answer":"H2O","category":1,"difficulty":2})
+        res = self.client().post('/questions/95',
+                                 json={
+                                       "question": "What is chemical \
+                                                   composition of water",
+                                       "answer": "H2O",
+                                       "category": 1,
+                                       "difficulty": 2
+                                     })
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 405)
         self.assertEqual(data['success'], False)
         self.assertEqual(data['message'], "Method Not Allowed.")
 
     def test_delete_question(self):
-        """Test API to delete question for question id passed as request argument(DELETE request). """
+        """Test API to delete question for question id passed
+        as request argument(DELETE request). """
         que = Question.query.filter(Question.question.ilike('%chemical%'))
         qid = que[0].id
         res = self.client().delete('/questions/'+str(que[0].id))
@@ -93,8 +110,8 @@ class TriviaTestCase(unittest.TestCase):
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
-        self.assertEqual(data['deleted'],qid)
-        self.assertEqual(question,None)
+        self.assertEqual(data['deleted'], qid)
+        self.assertEqual(question, None)
 
     def test_422_if_question_do_not_exist(self):
         """Test API to delete question with question id that do not exist """
@@ -104,10 +121,11 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(data['success'], False)
         self.assertEqual(data['message'], "unprocessable.")
 
-
     def test_search_questions(self):
-        """TEST API to search questions with search term as request argument """
-        res = self.client().post('/searchQuestions',json={"searchTerm":"title"})
+        """TEST API to search questions with search term as
+         request argument """
+        res = self.client().post('/searchQuestions',
+                                 json={"searchTerm": "title"})
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
@@ -115,26 +133,31 @@ class TriviaTestCase(unittest.TestCase):
         self.assertTrue(data['totalQuestions'])
 
     def test_get_questions_in_categories(self):
-       """Test API to get questions that are in request category"""
-       res = self.client().get('/categories/1/questions')
-       data = json.loads(res.data)
-       self.assertEqual(res.status_code, 200)
-       self.assertEqual(data['success'], True)
-       self.assertTrue(data['questions'])
-       self.assertTrue(data['total_questions'])
-       self.assertTrue(data['current_category'])
+        """Test API to get questions that are in request category"""
+        res = self.client().get('/categories/1/questions')
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['questions'])
+        self.assertTrue(data['total_questions'])
+        self.assertTrue(data['current_category'])
 
     def test_get_404_if_category_do_not_exist(self):
-         """Test API to get error if request category do not exist"""
-         res = self.client().get('/categories/10/questions')
-         data = json.loads(res.data)
-         self.assertEqual(res.status_code, 404)
-         self.assertEqual(data['success'], False)
-         self.assertEqual(data['message'],"Resource Not Found.")
+        """Test API to get error if request category do not exist"""
+        res = self.client().get('/categories/10/questions')
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], "Resource Not Found.")
 
     def test_play_trivia(self):
         """Test API to play 5 random trivia questions."""
-        res = self.client().post('/quizzes', json={"previous_questions":[],"quiz_category":{"type":"science","id":1}})
+        res = self.client().post('/quizzes', json={"previous_questions": [],
+                                                   "quiz_category": {
+                                                                     "type":
+                                                                     "science",
+                                                                     "id": 1
+                                                                     }})
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
@@ -142,11 +165,11 @@ class TriviaTestCase(unittest.TestCase):
 
     def test_500_if_request_is_not_formed_poperly(self):
         """Test API for error if request JSON is not formed properly"""
-        res = self.client().post('/quizzes', json={"previous_question":[]})
+        res = self.client().post('/quizzes', json={"previous_question": []})
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 422)
         self.assertEqual(data['success'], False)
-        self.assertEqual(data['message'],"unprocessable.")
+        self.assertEqual(data['message'], "unprocessable.")
 
 
 # Make the tests conveniently executable
